736e477ce4e44cc08cfb720a7356881ce47ff633
Óscar O <oscarforjuela@hotmail.com>
2023-05-24 16:44:05 -0500

    New Commit from MVC

---
+++ src/main/java/com/floatsystem/mvc/springboot/app/FloatSystemMvcApplication.java
@@ -0,0 +1,13 @@
+package com.floatsystem.mvc.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatSystemMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloatSystemMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floatsystem/mvc/springboot/app/FloatSystemMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floatsystem.mvc.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatSystemMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
